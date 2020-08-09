0a4ca9e35c78dab600a025d4ebd8b0ef21e3048d
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2020-08-09 09:39:57 +0000

    Remove requirements.txt

--- gradlew.bat
+++ gradlew.bat
@@ -25,3 +25,6 @@ if "%ERRORLEVEL%" == "0" goto init
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your J
