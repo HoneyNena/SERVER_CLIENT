709dfd4b017ca9d6dc1607b276b6727991d848d5
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2020-08-09 18:31:17 +0000

    ARM: kgdb: ignore breakpoint instructions from user mode

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,6 @@ echo location of your Java installation.
 goto fail
 
 :init
-@rem Get command-line a
+@rem Get command-line arguments, handling Windowz variants
+
+if not "%OS%" == "Windows_NT" goto win
