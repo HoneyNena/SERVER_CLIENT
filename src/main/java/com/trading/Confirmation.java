fc076e338e9b732f9b2b855d51c5fcdcba305e92
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2022-05-19 10:24:03 +0000

    [l10n] Update Japanese translation

--- src/main/java/com/trading/Confirmation.java
+++ src/main/java/com/trading/Confirmation.java
@@ -2,4 +2,9 @@ package com.trading;
 
 import com.google.common.base.MoreObjects;
 
-import java.io.Seri
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Confirmation implements Serializable {
+
+    p
