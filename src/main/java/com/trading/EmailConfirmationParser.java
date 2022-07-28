31d50765d448b5463298ee39c7f6a373f64c759e
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2022-07-28 12:57:36 +0000

    Swap else if for elseif

--- src/main/java/com/trading/EmailConfirmationParser.java
+++ src/main/java/com/trading/EmailConfirmationParser.java
@@ -5,4 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
-import java.time.LocalDa
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import 
