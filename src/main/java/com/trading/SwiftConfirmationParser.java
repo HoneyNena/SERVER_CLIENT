8f9f662605219f0dfd5535c06e2d61b0c2d1c82e
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2022-08-17 21:49:07 +0000

    Release v0.15.0

--- src/main/java/com/trading/SwiftConfirmationParser.java
+++ src/main/java/com/trading/SwiftConfirmationParser.java
@@ -39,3 +39,66 @@ public class SwiftConfirmationParser implements ConfirmationParser {
                 Field22A.tag(buySellTag(confirmation)),
                 currency(confirmation).asTag(),
                 quantity(confirmation).asTag(),
+                instrument(confirmation).asTag()
+        );
+    }
+
+    private static Field35B instrument(Confirmation confirmation) {
+        Field35B field35B = new Field35B();
+        field35B.setDescription(confirmation.getInstrumentName());
+        return field35B;
+    }
+
+    private static Field36B quantity(Confirmation confirmation) {
+        Field36B f36b = new Field36B();
+        f36b.setQualifier(MT518.CONF);
+        f36b.setQuantityTypeCode(UNIT_NUMBER);
+        f36b.setQuantity(Integer.toString(confirmation.getQuantity()));
+        return f36b;
+    }
+
+    private static Field11A currency(Confirmation confirmation) {
+        Field11A f11a = new Field11A();
+        f11a.setCurrency(confirmation.getInstrumentCurrency());
+        f11a.setQualifier(
+                "BUY".equals(confirmation.getTradeSide()) ? MT518.FXIB : MT518.FXIS
+        );
+
+        return f11a;
+    }
+
+    private static String buySellTag(Confirmation confirmation) {
+        return String.format(
+                ":%s/%s",
+                MT518.BUSE,
+                "BUY".equals(confirmation.getTradeSide()) ? "BUYI" : "SELL"
+        );
+    }
+
+    private static Field98A tradeDate(Confirmation confirmation) {
+        Field98A f98a = new Field98A();
+
+        LocalDate tradeDate = LocalDate.parse(
+                confirmation.getTradeDate(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
+
+        f98a.setDate(tradeDate.format(formatter));
+        f98a.setQualifier(MT518.TRAD);
+        return f98a;
+    }
+
+    private static Field90A price(Confirmation confirmation) {
+        Field90A f90a = new Field90A();
+        f90a.setPrice(confirmation.getPrice().toString());
+        return f90a;
+    }
+
+    private static MT518.SequenceA sequenceA() {
+        return MT518.SequenceA.newInstance(
+                Field20C.tag(":SEME//2005071800000923"),
+                Field23G.tag("NEWM"),
+                Field22F.tag(String.format(":TRTR//%s", MT518.TRAD))
+        );
+    }
+}
