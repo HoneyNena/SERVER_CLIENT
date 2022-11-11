b2013746a8cb05be608d47143423964f39d2fd3d
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2022-11-11 22:35:33 +0000

    Updating scripts

--- src/test/java/com/trading/ConfirmationSenderIntegrationTest.java
+++ src/test/java/com/trading/ConfirmationSenderIntegrationTest.java
@@ -1,3 +1,6 @@
 package com.trading;
 
-import com.fasterxml.jackson.databind.Obje
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.amqp.rabbit.connection.CachingC
