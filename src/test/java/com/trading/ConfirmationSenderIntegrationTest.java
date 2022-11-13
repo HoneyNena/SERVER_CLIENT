97f68176c483be0f43e2e471f94f505e729488f6
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2022-11-13 06:36:26 +0000

    Gemspec

--- src/test/java/com/trading/ConfirmationSenderIntegrationTest.java
+++ src/test/java/com/trading/ConfirmationSenderIntegrationTest.java
@@ -3,4 +3,6 @@ package com.trading;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.amqp.rabbit.connection.CachingC
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.s
