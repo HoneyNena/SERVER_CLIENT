35a2f3b07b9e6dd147d2501988053d188d97f587
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2022-11-13 12:46:39 +0000

    license.

--- src/test/java/com/trading/ConfirmationSenderIntegrationTest.java
+++ src/test/java/com/trading/ConfirmationSenderIntegrationTest.java
@@ -5,4 +5,5 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.s
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.cont
