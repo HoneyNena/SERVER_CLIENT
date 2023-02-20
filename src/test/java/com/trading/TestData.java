ea89c6c3f6f4c3eda2aed44c205d90c46927166a
HoneyNena <113073500+HoneyNena@users.noreply.github.com>
2023-02-20 07:20:28 +0000

    0.10.3

--- src/test/java/com/trading/TestData.java
+++ src/test/java/com/trading/TestData.java
@@ -5,4 +5,7 @@ import java.math.BigDecimal;
 class TestData {
 
     static Confirmation confirmation(String allocationId) {
-       
+        return confirmation(allocationId, "BUY");
+    }
+
+    static Confirmati
